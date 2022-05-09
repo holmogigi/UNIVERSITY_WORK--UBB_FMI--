a3cabffab1236c24e9ba2e83337a5a3ad35dcf5d
913-Halmagyi-Filip_Nicholas <91518547+holmogigi@users.noreply.github.com>
2022-05-09 13:54:24 +0300

    Add files via upload

---
+++ 1'st SEMESTER/PF (Programming Fundamentals)/A2/program.py
@@ -0,0 +1,17 @@
+#
+# Write the implementation for A2 in this file
+#
+
+# UI section
+# (write all functions that have input or print statements here). 
+# Ideally, this section should not contain any calculations relevant to program functionalities
+
+print('Hello A2e1')
+
+# Function section
+# (write all non-UI functions in this section)
+# There should be no print or input statements below this comment
+# Each function should do one thing only
+# Functions communicate using input parameters and their return values
+
+# print('Hello A2'!) -> prints aren't allowed here!
