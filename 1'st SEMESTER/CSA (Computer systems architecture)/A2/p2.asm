e6e0af2a0b6e892873ffa777a7b4e31e346bf91f
Halmagyi-Filip Nicholas <91518547+holmogigi@users.noreply.github.com>
2022-11-17 14:28:09 +0200

    Create p2.asm

---
+++ 1'st SEMESTER/CSA (Computer systems architecture)/A2/p2.asm
@@ -0,0 +1,25 @@
+bits 32 
+
+global start        
+
+extern exit               
+import exit msvcrt.dll 
+
+segment data use32 class=data
+a db 01;
+b db 02;
+c db 03;
+d db 04;
+
+segment code use32 class=code
+    start:
+        ;(a+a)-(b+b)-c
+        mov Al, 0;
+        add Al, [a];
+        add Al, [a];
+        sub Al, [b];
+        sub Al, [b];
+        sub Al, [c];
+        
+        push    dword 0      
+        call    [exit]       
