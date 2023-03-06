469716a3ed64be6ef737efb2d4dd7cb536de8086
Halmagyi-Filip Nicholas <91518547+holmogigi@users.noreply.github.com>
2023-03-06 17:05:03 +0200

    Add files via upload

---
+++ 4'th SEMESTER/SDI (Systems for Design and Implementation)/A1/WebApplication2/WebApplication2/BodyBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2;
+
+public class BodyBuilder
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public int Weight { get; set; }
+    public int Height { get; set; }
+    public string? Division { get; set; }
+    
+}
---
+++ 4'th SEMESTER/SDI (Systems for Design and Implementation)/A1/WebApplication2/WebApplication2/Database.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication2;
+
+public class Database : DbContext
+{
+    public Database(DbContextOptions<Database> options)
+        : base(options)
+    {}
+
+    public DbSet<BodyBuilder> BodyBuilders { get; set; } = null!;
+
+}
