cedea44f1685842ab9ec55e35aa729b6c636b2cd
Halmagyi-Filip Nicholas <91518547+holmogigi@users.noreply.github.com>
2024-03-11 10:52:24 +0200

    Create dividffdouble.m

---
+++ 6'th SEMESTER/NC (Numerical Calculus)/LAB3/dividffdouble.m
@@ -0,0 +1,13 @@
+function [z, td] = dividffdouble(x, f, fd)
+    n = length(x);
+    z = zeros(1, 2*n);
+    lz = 2*n;
+    z = repelem(x, 2);
+    td = zeros(lz);
+    td(1:2:lz-1) = fd';
+    td(2:2:lz-2,2) = (diff(f)./diff(x))';
+    td(1:lz, 1) = repelem(f, 2)';
+    for j = 3:lz
+        td(1:lz-j+1, j) = diff(td(1:lz-j+2, j-1))./(z(j:lz)-z(1:lz-j+1))';
+    end
+end
