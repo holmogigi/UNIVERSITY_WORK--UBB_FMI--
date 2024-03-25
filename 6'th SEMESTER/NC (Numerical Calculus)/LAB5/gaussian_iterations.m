d64cecbae8b500878edd250a4cee483e1cd20b34
Halmagyi-Filip Nicholas <91518547+holmogigi@users.noreply.github.com>
2024-03-25 11:13:29 +0200

    Create gaussian_iterations.m

---
+++ 6'th SEMESTER/NC (Numerical Calculus)/LAB5/gaussian_iterations.m
@@ -0,0 +1,14 @@
+function [x, nit] = gaussian_iterations(A, b, x0, maxit, epsilon)
+    N = -triu(A, 1);
+    M = tril(A);
+    T = inv(M)*N;
+    c = inv(M)*b;
+    alfa = norm(T, inf);
+    for nit = 1:maxit
+        x = T*x0+c;
+        if norm(x-x0, inf) < epsilon* (1-alfa)/alfa
+            return
+        end
+        x0=x;
+    end
+end
