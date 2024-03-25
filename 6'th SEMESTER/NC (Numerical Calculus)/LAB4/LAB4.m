b45ff39281a16745a34c49e2cec0eb1d8bc15a6e
Halmagyi-Filip Nicholas <91518547+holmogigi@users.noreply.github.com>
2024-03-25 10:54:17 +0200

    Update LAB4.m

--- 6'th SEMESTER/NC (Numerical Calculus)/LAB4/LAB4.m
+++ 6'th SEMESTER/NC (Numerical Calculus)/LAB4/LAB4.m
@@ -1,28 +1,19 @@
-﻿% 1. a)
-x = [0,1,2];
-f = [1,1/2,1/3];
-divided_diff(x, f)
+U= [2,4,2;0,-1,1;0,0,-1]
+b= [8;0;-1]
 
-%1. b)
-x = [0,1,2];
-f = [1,1/2,1/3];
-df = -1./(1+x).^2;
-[z, t] = divided_diff2(x, f, df)
+backward_subs(U,b)
 
-%1. c)
-x = linspace(1,2,11);
-f = 1./(1+x);
-df = -1./(1+x).^2;
-divided_diff(x, f)
-[z, t] = divided_diff2(x, f, df)
+L= [1,0,0;1/2,1,0;1/2,1,1]
+b= [8;4;3]
 
-%2. a)
-x = [-2,-1,0,1,2,3,4];
-f = [-5,1,1,1,7,25,60];
-divided_diff(x, f)
+forward_subs(L,b)
 
-%2. b)
-forward_diff(f)
-    
-%2. c)
-backward_diff(f)
+A= [2,1,-1,-2;4,4,1,3;-6,-1,10,10;-2,1,8,4]
+b= [2;4;-5;1]
+[L,U,P] = lu(A);
+y= forward_subs(L,P*b)
+x= backward_subs(U,y)
+
+A= [2,1,-1,-2;4,4,1,3;-6,-1,10,10;-2,1,8,4]
+b= [2;4;-5;1]
+gauss_pivot(A,b)
