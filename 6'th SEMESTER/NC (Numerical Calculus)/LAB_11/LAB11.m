e22e6c9436d936b2596dd3a845bcf11ad00d897a
Halmagyi-Filip Nicholas <91518547+holmogigi@users.noreply.github.com>
2024-05-13 11:52:11 +0300

    Update LAB11.m

--- 6'th SEMESTER/NC (Numerical Calculus)/LAB_11/LAB11.m
+++ 6'th SEMESTER/NC (Numerical Calculus)/LAB_11/LAB11.m
@@ -1,1 +1,12 @@
-s
+vpa(pi*sqrt(3)/9, 6)
+[I, nfev] = ronberg(@(x)1./(2+sin(x)), 0, pi/2, 10^-5, 100)
+
+
+gaussquad(@(x)x.^2+1./(x.^2+1), 2, 3) * 2
+
+
+integral(@(x) exp(cos(x)), 0, pi/4)
+gaussquad(@(x) exp(cos(x)), 5, 1)
+
+
+gaussquad(@(x)exp(-x).^2.*cos(x), 6, 1)
